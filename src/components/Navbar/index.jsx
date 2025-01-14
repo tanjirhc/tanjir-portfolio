6e719fc0fa34f90008f65edfbddcee4de2fb3eb4
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-14 17:03:33 +0600

    btn importing files

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,35 +1,50 @@
-import React from 'react';
-import './Navbar.css';
-import Logo from '../../assets/Logo';
-import { FaTimes } from 'react-icons/fa';
-import {menu} from '../../data';
-import {Link} from 'react-scroll';
-
+import React from "react";
+import "./Navbar.css";
+import Logo from "../../assets/Logo";
+import { FaTimes } from "react-icons/fa";
+import { menu } from "../../data";
+import { Link } from "react-scroll";
+import { FaArrowRightFromSquare } from "react-icons/fa";    // Add this line
 
 const Navbar = () => {
-    return (
-        <nav className="card flex__center navbar">
-            <div className="flex__center logo">
-                <Logo />
-            </div>
+  return (
+    <nav className="card flex__center navbar">
+      <div className="flex__center logo">
+        <Logo />
+      </div>
 
-            <aside className="flex__center sidebar">
-                <div className="flex sidebar__top">
-                    <span className="icon__container close__btn">
-                        <FaTimes />
-                    </span>
-                </div>
-                <div className="flex sidebar__middle">
-                    {
-                        menu.map((list, index) => {
-                            <Link to={list.name.toLowerCase()}>{list.name}</Link>
-                        })
-                    }
-                    
-                </div>
-            </aside>
-        </nav>
-    );
+      <aside className="flex__center sidebar">
+        <div className="flex sidebar__top">
+          <span className="icon__container close__btn">
+            <FaTimes />
+          </span>
+        </div>
+        <div className="flex sidebar__middle">
+          {menu.map((list, index) => {
+            <Link
+              to={list.name.toLowerCase()}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              key={index}
+              className="tab"
+              activeClass="btn__shine"
+            >
+              {list.name}
+            </Link>;
+          })}
+        </div>
+      </aside>
+      <div className="flex__container buttons__wrapper">
+        <Link to="contact" className="btn flex__center hire__btn" spy={true} smooth={true} offset={-70} duration={500}>
+            Hire Me <div className="flex__center icon">
+                <FaArrowRightFromSquare />
+            </div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
