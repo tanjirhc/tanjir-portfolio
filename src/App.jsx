46ac216f947f8dd231ca5891d990aea2379a6ec1
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-14 16:15:21 +0600

    All components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import About from "./components/About";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Services from "./components/Services";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Skills />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   );
 };
