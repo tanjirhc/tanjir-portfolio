d08ed48b41b1b6b7186227732776ead0f0a1cc71
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-14 16:36:16 +0600

    Navbar Design

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Navbar.css';
+import Logo from '../../assets/Logo';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar fff
-        </div>
+        <nav className="card flex__center navbar">
+            <div className="flex__center logo">
+                <Logo />
+            </div>
+
+            <aside className="flex__center sidebar">
+                <a href="#about">About</a>
+                <a href="#services">Services</a>
+                <a href="#skills">Skills</a>
+                <a href="#projects">Projects</a>
+                <a href="#testimonials">Testimonials</a>
+                <a href="#contact">Contact</a>
+            </aside>
+        </nav>
     );
 };
 
